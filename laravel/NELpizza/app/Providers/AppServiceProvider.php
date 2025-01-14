cb3c35942895194dff92b304f61b5f599619a55d
Lowkey <113860192+56snty@users.noreply.github.com>
2025-01-14 10:51:45 +0100

    Bestel pagina werkt

--- laravel/NELpizza/app/Providers/AppServiceProvider.php
+++ laravel/NELpizza/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Hier kun je eventuele bindingen of services registreren als dat nodig is
     }
 
     /**
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Beperkt de standaard stringlengte tot 191 karakters
+        Schema::defaultStringLength(191);
     }
 }
