794937f5f042bc856d2b1071356194531058c2d0
anatomyking <ps219772@summacollege.nl>
2025-01-15 20:20:47 +0100

    home page

--- laravel/NELpizza/resources/views/layouts/app.blade.php
+++ laravel/NELpizza/resources/views/layouts/app.blade.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Stonks Pizza')</title>
     <link rel="stylesheet" href="{{ asset('css/navi.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/navi2.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <script src="{{ asset('js/javascript.js') }}" defer></script>
 </head>
 <body>
@@ -25,9 +22,11 @@
             <a href="{{ route('contact') }}" id="two" class="item" >Contact</a>
             <a href="{{ route('dashboard') }}" id="three" class="item">Login</a>
         </nav>
-        <main>
-            @yield('content')
-        </main>
+        <div class="contentview">
+        @yield('content')
+        
+        </div>
     </div>
 </body>
+
 </html>
