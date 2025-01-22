d3386b09cecc6f3ff770824f380e5546fcde93a4
mistervambree <159008333+mistervambree@users.noreply.github.com>
2025-01-22 13:27:19 +0100

    progressbar
    progressbar

--- laravel/NELpizza/resources/views/status.blade.php
+++ laravel/NELpizza/resources/views/status.blade.php
@@ -3,6 +3,24 @@
 @section('title', 'status')
 @section('refrence')
 <link rel="stylesheet" href="{{ asset('css/status.css') }}">
+<script src="{{ asset('js/status.js') }}" defer></script>
 @endsection
 @section('menu-title', 'Contact Information')
 
+<main class="main-wrapper">
+  <div class="steps-wrapper">
+    <div class="steps">
+      <span class="step active">1</span>
+      <span class="step">2</span>
+      <span class="step">3</span>
+      <span class="step">4</span>
+      <div class="progress-bar">
+        <span class="progress"></span>
+      </div>
+    </div>
+    <div class="buttons">
+      <button class="btn btn-prev" id="btn-prev" disabled>Previous</button>
+      <button class="btn btn-next" id="btn-next">Next</button>
+    </div>
+  </div>
+</main>
